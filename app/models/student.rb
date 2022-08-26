b9780dd891c3163960083578f93edb8cae041971
Mwenje <mwenje24@gmail.com>
2022-08-26 13:26:21 +0300

    commit tree f97301fbbb04386ce154aaa4ed866c626e9cef74

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,17 @@
+# ActiveRecord::Base.establish_connection(
+#     adapter: "sqlite3",
+#     database: "db/development.sqlite3"
+# )
+
+# sql = <<-SQL
+#   CREATE TABLE IF NOT EXISTS students (
+#   id INTEGER PRIMARY KEY,
+#   name TEXT,
+#   )
+# SQL
+
+# ActiveRecord::Base.connection.execute(sql)
+
+
 class Student < ActiveRecord::Base
 end
