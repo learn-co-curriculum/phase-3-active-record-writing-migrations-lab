f41842c63b48a1f508828679e9dba6d32691374f
Meshack Korir <meshackkorir100@gmail.com>
2023-06-03 22:55:05 +0300

    just published

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,10 @@
 
 ActiveRecord::Schema.define(version: 1) do
 
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
