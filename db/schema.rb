7045db2b90dcc4254ad79e95f8109efcd2be29c0
Meshack Korir <meshackkorir100@gmail.com>
2023-06-04 13:58:39 +0300

    added columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2023_06_04_104430) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "grade"
+    t.string "birthdate"
   end
 
 end
