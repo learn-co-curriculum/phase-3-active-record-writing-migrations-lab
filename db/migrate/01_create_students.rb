07ba2bd258c124607bffc4154b90aa7ee3d91991
Meshack Korir <meshackkorir100@gmail.com>
2023-06-04 13:10:58 +0300

    created table

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -2,7 +2,7 @@ class CreateStudents < ActiveRecord::Migration[6.1]
   def change
     create_table :students do |t|
       t.string :name
-      t.timesstamps
+      t.timestamps
     end
   end
 end
